7274d586691259eb8ca566af323c98226e261104
notGonim <mhmodgonim96@gmail.com>
2021-04-25 00:28:11 +0200

    ADDED the search component strucutre

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();
 
-  constructor() { }
+  term = '';
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(event: any) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
   }
-
 }
