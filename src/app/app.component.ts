93e9926d0fa0f7fec84dd0a63813aea823a013b6
notGonim <mhmodgonim96@gmail.com>
2021-04-25 00:50:08 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikiService } from './wiki.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  page = [];
+  constructor(private Wiki: WikiService) {}
+
   onTerm(term: string) {
-    console.log('term');
+    this.Wiki.search(term).subscribe((res:any) => {
+      this.page = res.query.search;
+    });
   }
 }
